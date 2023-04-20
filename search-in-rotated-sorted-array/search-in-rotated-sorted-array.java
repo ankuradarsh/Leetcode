01d55b76b4bfcf8c30e1b37c5e3ec01bab6f55c8
Ankur Adarsh <77184810+ankuradarsh@users.noreply.github.com>
2023-04-20 10:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.99%) - LeetSync

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,20 +1,20 @@
 class Solution {
     public int search(int[] a, int target) {
-        int start=0, end=a.length-1;
+        // int start=0, end=a.length-1;
         // reverse(a, 0, end);
 
-        for(int i=0;i<=end;i++){
+        for(int i=0;i<a.length;i++){
             if(target == a[i]) return i;
         }
         return -1;
     }
-    public void reverse(int a[], int start, int end){
-        while(start < end){
-            int temp = a[start];
-            a[start] = a[end];
-            a[end] = temp;
-            start++;
-            end--;
-        }
-    }
+    // public void reverse(int a[], int start, int end){
+    //     while(start < end){
+    //         int temp = a[start];
+    //         a[start] = a[end];
+    //         a[end] = temp;
+    //         start++;
+    //         end--;
+    //     }
+    // }
 }
