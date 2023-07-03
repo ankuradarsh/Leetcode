bb044dd1e7f44cc2aa7a4c0885ed65d0069dfed7
Ankur Adarsh <77184810+ankuradarsh@users.noreply.github.com>
2023-07-03 09:53:06 +0530

    Time: 1 ms (100.00%) | Memory: 40.2 MB (28.99%) - LeetSync

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -2,17 +2,17 @@ class Solution {
     public int reverse(int x) {
         long n=x;
         long num=0;
-        // if(x<0){
-        //     n = Math.abs(n);
-        // }
+        if(x<0){
+            n = Math.abs(n);
+        }
         while(n!=0){
             long mod = n%10;
             num = num*10+mod;
             n = n/10;
         }
-        // if(x<0){
-        //     num = -1*num;
-        // }
+        if(x<0){
+            num = -1*num;
+        }
 
         if(num>Math.pow(2,31)-1 || num<-(Math.pow(2,31))) return 0;
         else return (int)num;
